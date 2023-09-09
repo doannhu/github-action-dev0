75201683fa8f4ac524a167c8f8f26ed9ab07facd
doannhu <ndodoan@gmail.com>
2023-09-09 13:35:38 +1000

    add comment

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/<random_string>')
 def reversed_string(random_string):
+    # comment on this function
     return "".join(reversed(random_string))
 
 @app.route('/get-mode')
